5574772c739fd586455c1a12591bdf5e977b6431
Mihai Morariu <mihaimorariu@gmail.com>
2012-10-01 22:11:40 +0200

    Added screenshots of phantom histogram + code for computing volumes

--- CoralVolume2.py
+++ CoralVolume2.py
@@ -1,23 +1,76 @@
 import vtk
 
-reader = vtk.vtkXMLImageDataReader()
-reader.SetFileName('/media/Documents/UvA/Scientific Visualization and Virtual Reality/S1_CroppedSamples/S1_488.vti')
-reader.Update()
+file_path = '/media/Documents/UvA/Scientific Visualization and Virtual Reality/S1_CroppedSamples/S1_480.vti'
+threshold = [-700, 1800]
 
-thresh = vtk.vtkImageThreshold()
-thresh.SetInputConnection(reader.GetOutputPort())
-thresh.ThresholdBetween(-700, 2900)
-thresh.SetInValue(1)
-thresh.SetOutValue(0)
-thresh.Update()
+def loadData():
+    reader = vtk.vtkXMLImageDataReader()
+    reader.SetFileName(file_path)
+    reader.Update()
 
-iso = vtk.vtkImageMarchingCubes()
-iso.SetInputConnection(thresh.GetOutputPort())
-iso.Update()
+    return reader
+    
+def generateIsosurface(reader):
+    isoSurface = vtk.vtkContourFilter()
+    isoSurface.SetInputConnection(reader.GetOutputPort())
+    isoSurface.GenerateValues(3, threshold)
+    
+    return isoSurface
 
-mass = vtk.vtkMassProperties()
-mass.SetInputConnection(iso.GetOutputPort())
-mass.Update()
+def getMassProperties(triangles):
+    mass = vtk.vtkMassProperties()
+    mass.SetInputConnection(triangles.GetOutputPort())
+    
+    return mass.GetVolume(), mass.GetSurfaceArea()
+    
+def dataCleanup(surface):
+    clean_data = vtk.vtkCleanPolyData()
+    clean_data.SetInputConnection(surface.GetOutputPort())
+    
+    return clean_data
+    
+def createGeometry(surface):
+    mapper = vtk.vtkPolyDataMapper()
+    mapper.SetInputConnection(surface.GetOutputPort())
+    
+    actor = vtk.vtkActor()
+    actor.SetMapper(mapper)
+    
+    return actor
+    
+def triangulateData(surface):
+    triangles = vtk.vtkTriangleFilter()
+    triangles.SetInputConnection(surface.GetOutputPort())
+    
+    return triangles
+    
+def runVisualization(actor):
+    renderer = vtk.vtkRenderer()
+    renderer.AddActor(actor)
+    renderer.SetBackground(1, 0.25, 0.25)
+    
+    window = vtk.vtkRenderWindow()
+    window.AddRenderer(renderer)
+    
+    iren = vtk.vtkRenderWindowInteractor()
+    iren.SetRenderWindow(window)
+    
+    window.Render()
+    iren.Start()
 
-print "Volume = ", mass.GetVolume() 
-print "Surface = ", mass.GetSurfaceArea()
+def runMain():
+    reader = loadData()
+    isoSurface = generateIsosurface(reader)
+    cleanSurface = dataCleanup(isoSurface)
+    triangSurface = triangulateData(cleanSurface)
+    actor = createGeometry(triangSurface)
+    
+    [volume, surface] = getMassProperties(cleanSurface)
+    
+    print "Volume:", volume
+    print "Surface:", surface
+    
+    runVisualization(actor)
+    
+if __name__ == "__main__":
+    runMain()
