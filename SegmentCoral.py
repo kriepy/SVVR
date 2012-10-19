861ae8f80517fc11be2e913c8ccad424da50d7e3
toby.hijzen@gmail.com <toby@ubuntu.(none)>
2012-10-19 16:06:16 +0200

    added selector

--- SegmentCoral.py
+++ SegmentCoral.py
@@ -2,6 +2,7 @@ import vtk
 
 file_path = '../Data/1000_KORAALS1/20110617/00006_POLS_0_5_MM'
 threshold = [-700, -700]
+n_regions = 0
 
 # Load the data from a folder containing DICOM images
 def loadData():
@@ -12,15 +13,36 @@ def loadData():
     imageData.DeepCopy(reader.GetOutput())
     return reader
 
+def select(connected):
+    selector = vtk.vtkThresholdPoints()
+    selector.SetInput(connected.GetOutput())
+    print n_regions
+    for i in range(10,11):#change to n_regions
+        selector.ThresholdBetween(48,48)
+        selector.SetInputArrayToProcess(1, 0, 0, vtk.vtkDataObject.FIELD_ASSOCIATION_CELLS, "RegionId" )
+        selector.Update()
+        triangles = vtk.vtkTriangleFilter()
+        triangles.SetInputConnection(selector.GetOutputPort())
+        [V, A] = getMassProperties(triangles)
+        print ("The volume is: " + str(V) + "\n")
+    return selector
+
 def connectivityFilter(polyData):
     connectivityFilter = vtk.vtkPolyDataConnectivityFilter()
     connectivityFilter.SetInputConnection(polyData.GetOutputPort())
     connectivityFilter.SetExtractionModeToAllRegions()
-    #connectivityFilter.ColorRegionsOn()
+    connectivityFilter.ColorRegionsOn()
     connectivityFilter.Update()
-    print ("number of regions extracted: " + str(connectivityFilter.GetNumberOfExtractedRegions()))
-
+    global n_regions
+    n_regions = connectivityFilter.GetNumberOfExtractedRegions()
+    print ("number of regions extracted: " + str(n_regions) + "\n")
     return connectivityFilter
+
+def getMassProperties(triangles):
+    mass = vtk.vtkMassProperties()
+    mass.SetInputConnection(triangles.GetOutputPort())
+    
+    return mass.GetVolume(), mass.GetSurfaceArea()
 
 # Generate the Isosurface using pre-specified threshold
 def generateIsosurface(reader):
@@ -66,7 +88,8 @@ def runMain():
     reader = loadData()
     isoSurface = generateIsosurface(reader)
     connected = connectivityFilter(isoSurface)
-    triangSurface = triangulateData(connected)
+    selected = select(connected)
+    triangSurface = triangulateData(selected)
     actor = createGeometry(triangSurface)
 
     runVisualization(actor)
